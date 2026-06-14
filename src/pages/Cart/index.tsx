6bbf55c1729e1336bd82654507763a87420177a4
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2026-06-14 11:46:54 -0300

    featc: cria as pasta incial do projeto

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,12 @@
+
+import style from './cart.module.scss';
+
+function Cart(){
+    return (
+        <div className={style.cart}>
+            <h1>Cart</h1>
+        </div>
+    )
+}
+
+export default Cart;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,20 @@
 //criando rotas para o projeto
 
-function MainRoutes(){
-    return(
-        <Routes>
-            <Route element={<App/>} path="/" />
+import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Success from "./pages/Success";
 
+function MainRoutes(){
+    return (
+        <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Cart />} path="/cart" />
+            <Route element={<Product />} path="/product/:id" />
+            <Route element={<Success />} path="/success" />
         </Routes>
-    )
+    );
 }
 
 export default MainRoutes;
