3f3ab23af2ee6e8cc5c0ca6e05dd471d41be2e63
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2026-06-14 10:50:47 -0300

    Chore: Configuraçao incial do App

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+//criando rotas para o projeto
+
+function MainRoutes(){
+    return(
+        <Routes>
+            <Route element={<App/>} path="/" />
+
+        </Routes>
+    )
+}
+
+export default MainRoutes;
